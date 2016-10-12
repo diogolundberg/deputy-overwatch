c7239382b552de549b713445966e9aa999e97f65
Diogo Lundberg <diogo@lundberg.com.br>
2016-10-12 19:26:14 -0300

    Adds flask testing and base test case

---
+++ tests/test_base.py
@@ -0,0 +1,10 @@
+from overwatch import app
+from flask.ext.testing import TestCase
+
+
+class BaseTestCase(TestCase):
+
+    def create_app(self):
+        app.testing = True
+        app.debug = True
+        return app
