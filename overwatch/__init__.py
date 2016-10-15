7cefe8893060710b7897c29b24bc0d9c586d40cd
Diogo Lundberg <diogo@lundberg.com.br>
2016-10-15 14:22:31 -0300

    Switches pattern.en for inflection

--- overwatch/__init__.py
+++ overwatch/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from os import getenv, listdir, getcwd, path
 from importlib import import_module
 from re import sub
-from pattern.en import pluralize, singularize
+from inflection import singularize
 
 app = Flask(__name__)
 app.config.from_object('config.' + getenv('ENV', 'Development'))
