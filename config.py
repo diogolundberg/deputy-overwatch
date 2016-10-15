f7848f6926d7f5d872300723ff00cadce669a5e9
Diogo Lundberg <diogo@lundberg.com.br>
2016-10-15 07:46:54 -0300

    Fixes sqlite absolute path

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+from os import getcwd, path
+
 
 class Config(object):
     DEBUG = False
@@ -9,7 +11,7 @@ class Production(Config):
 
 class Development(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///overwatch_develop.db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + path.join(getcwd(), 'overwatch_develop.db')
 
 class Testing(Config):
     DEBUG = True
