2ff0755aad37a1e46bb1cd4c72d3fb3a7a862f28
Diogo Lundberg <diogo@lundberg.com.br>
2016-10-15 22:14:11 -0300

    Fixes deputy loader scripts

--- overwatch/scripts/deputy_loader.py
+++ overwatch/scripts/deputy_loader.py
@@ -1,12 +1,13 @@
 from overwatch.models import Deputy
-from overwatch.scripts import parse_data
+from overwatch.scripts import scrapper
 from overwatch import db
 
-def update_deputies():
-    url = ("http://dadosabertos.almg.gov.br/ws/deputados/em_exercicio")
-    xml = parse_data.get_xml_parsed_data(url)
-    deputies = xml['listaDeputado']['deputado']
 
+def update_deputies():
+    url = "http://dadosabertos.almg.gov.br/ws/deputados/em_exercicio"
+    xml_dict = scrapper.parse_xml(url)[0]
+
+    deputies = xml_dict['listaDeputado']['deputado']
     for d in deputies:
         deputy = Deputy()
         deputy.name = d['nome']
--- tests/scripts/test_deputy_loader.py
+++ tests/scripts/test_deputy_loader.py
@@ -8,29 +8,28 @@ class DeputyLoaderTests(BaseTestCase):
 
     def setUp(self):
         deputy_loader.update_deputies()
-        self.deputies = [dict(d) for d in Deputy.query.all()]
+        self.deputies = Deputy.query.all()
 
 
     def test_should_load_77_deputies(self):
-        deputy = self.deputies[0]
         self.assertEqual(len(self.deputies), 77)
 
 
     def test_every_deputy_should_have_name(self):
         deputy = self.deputies[0]
-        self.assertTrue(deputy['name'])
+        self.assertTrue(deputy.name)
 
 
     def test_every_deputy_should_have_id(self):
         deputy = self.deputies[0]
-        self.assertTrue(deputy['id'])
+        self.assertTrue(deputy.id)
 
 
     def test_every_deputy_should_have_party(self):
         deputy = self.deputies[0]
-        self.assertTrue(deputy['party'])
+        self.assertTrue(deputy.party)
 
 
     def test_should_overwrite_existing_data(self):
         deputy_loader.update_deputies()
-        self.assertEqual(len(self.deputies), 77)
+        self.assertEqual(len(Deputy.query.all()), 77)
