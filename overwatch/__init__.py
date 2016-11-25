0c8f7908d91c27116288874fe8b4fab58f195bc4
Diogo Lundberg <diogo@lundberg.com.br>
2016-11-25 15:42:31 -0200

    pep8

--- overwatch/__init__.py
+++ overwatch/__init__.py
@@ -1,20 +1,23 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from os import getenv, listdir, getcwd, path
 from importlib import import_module
 from re import sub
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask.ext.runner import Manager
 from inflection import singularize
-
 
 app = Flask(__name__, template_folder=path.join(getcwd(), __name__, 'views'))
 app.config.from_object('config.' + getenv('ENV', 'Development'))
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+manager = Manager(app)
 
 
 def register_blueprints(package):
     package_dir = path.join(getcwd(), __name__, package)
-    module_suffix = '_'+singularize(package)+'.py'
+    module_suffix = '_' + singularize(package) + '.py'
 
     module_names = [sub('\.py$', '', c)
                     for c in listdir(package_dir) if c.endswith(module_suffix)]
