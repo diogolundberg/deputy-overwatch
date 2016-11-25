15955338bd192f5731ab26a7df28d6230b16a888
Diogo Lundberg <diogo@lundberg.com.br>
2016-11-25 15:42:23 -0200

    renames run.py

--- manage.py
+++ manage.py
@@ -1,11 +1,7 @@
-from overwatch import app, db, models
+from flask_migrate import MigrateCommand
+from overwatch import manager
 from overwatch.scripts import Scrape
-from flask.ext.runner import Manager
-from flask_migrate import MigrateCommand, Migrate
 
-migrate = Migrate(app, db)
-manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 manager.add_command('scrape', Scrape)
-
 manager.run()
